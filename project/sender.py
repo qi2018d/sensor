38a2ea7fd627a5e9735ff138d9c74bb27e344eed
Mookeun Ji <goofcode@gmail.com>
2018-02-15 09:59:11 -0800

    Change uuid, Add ready.sh

--- project/sender.py
+++ project/sender.py
@@ -1,6 +1,8 @@
 import json
 import threading
+
 from db_manager import DBManager
+
 
 class Sender:
 
@@ -18,22 +20,20 @@ class Sender:
 
         self.lock.acquire()
         for handler in self.active_handlers:
-            json_string = json.dumps(data, cls= self.DataEncoder)
-            handler.send(json_string)
+            handler.send(json.dumps(data, cls= self.DataEncoder))
         self.lock.release()
 
     def send_air_history_from(self, timestamp):
 
+        data = DBManager.get_air_data_from(timestamp)
         chunk_size = 5
 
-        data = DBManager.get_air_data_from(timestamp)
-
-        # send historical data if exists
+        # chop chop chop in chunk with size of chunk_size
         if data is not None:
             data_chunks = [data[i:i + chunk_size] for i in xrange(0, len(data), chunk_size)]
 
             for chunk in data_chunks:
-                self.send({'type': 'historical', 'data': chunk})
-
-    def __format(self, data):
-        pass
+                self.lock.acquire()
+                for handler in self.active_handlers:
+                    handler.send(json.dumps({'type': 'historical', 'data': chunk}, cls=self.DataEncoder))
+                self.lock.release()
